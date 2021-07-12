ce5543384c7da1aa7f1c795795711ab170c1e41b
pampapathi73 <pampapathik32@gmail.com>
2021-07-12 19:17:15 +0530

    [pampapathi] : FunctionDailyHours

--- JavaScript_EmployeeWage/EmpWage.js
+++ JavaScript_EmployeeWage/EmpWage.js
@@ -8,7 +8,7 @@ else
     console.log("Employee Is PRESENT");
     */
 
-    //UC-2
+    //UC-3
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -19,16 +19,17 @@ const NUM_OF_WORKING_DAYS=2;
 
 let empHrs = 0; empHrs = 4 
 let empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck){
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOUR;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOUR;
-        break;
-    default:
-        empHrs = 0;
-
+// fun
+function getWorkingHours(empCheck){
+    switch (empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOUR;            
+        case IS_FULL_TIME:
+            return FULL_TIME_HOUR;            
+        default:
+            empHrs = 0;
+    }
 }
 let empWage = empHrs * WAGE_PER_HOUR;
+empHrs = getWorkingHours(empWage);
 console.log("Emp Wage: " + empWage);
