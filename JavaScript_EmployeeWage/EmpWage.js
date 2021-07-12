f31ae02678cf15f02f3998ec35aa5243f6005fb6
pampapathi73 <pampapathik32@gmail.com>
2021-07-12 20:25:52 +0530

    [pampapathi] : Store Daily wage

--- JavaScript_EmployeeWage/EmpWage.js
+++ JavaScript_EmployeeWage/EmpWage.js
@@ -8,20 +8,33 @@ else
     console.log("Employee Is PRESENT");
     */
 
-    //UC-4
+//UC-6
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOUR = 4;
 const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS=2;
 
-let empHrs = 0; empHrs = 4 
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);
+const NUM_OF_WORKING_DAYS=10;
+const MAX_HRS_IN_MONTH = 100;
+let empHrs = 0; 
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+
+let empDailyWageArr = new Array();
+
+function calcDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
 }
+while ( totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+}   
+
 // fun
 function getWorkingHours(empCheck){
     switch (empCheck){
@@ -33,6 +46,6 @@ function getWorkingHours(empCheck){
             return 0;
     }
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-empHrs = getWorkingHours(empWage);
-console.log(" Total Hrs: " + empHrs + " Emp Wage: " + empWage);
+//Computation
+let empWage = calcDailyWage(totalEmpHrs);
+console.log(" Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
