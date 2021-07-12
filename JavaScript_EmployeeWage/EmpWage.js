094b4553d9654fd27b17319ad115e8dcc6f6f385
Pampapathi K <83532368+pampapathi73@users.noreply.github.com>
2021-07-12 19:34:51 +0530

    Update EmpWage.js

--- JavaScript_EmployeeWage/EmpWage.js
+++ JavaScript_EmployeeWage/EmpWage.js
@@ -17,7 +17,7 @@ const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS=2;
 
-let empHrs = 0; empHrs = 4 
+let empHrs = 0; 
 for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
     let empCheck = Math.floor(Math.random() * 10) % 3;
     empHrs += getWorkingHours(empCheck);
